0b9a944c6975449c885e5897784a3009c7afbc04
Ricardo Trujillo <rtrujillo@esri.com>
2014-10-30 14:08:59 -0700

    Initial 10.3.0 upgrade

--- hdfs-transport/src/main/java/com/esri/geoevent/transport/hdfs/HDFSConnection.java
+++ hdfs-transport/src/main/java/com/esri/geoevent/transport/hdfs/HDFSConnection.java
@@ -20,9 +20,13 @@
   Redlands, California, USA 92373
 
   email: contracts@esri.com
-*/
+ */
 
 package com.esri.geoevent.transport.hdfs;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataOutputStream;
@@ -31,114 +35,115 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hdfs.DistributedFileSystem;
 import org.apache.hadoop.io.IOUtils;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
+import com.esri.ges.framework.i18n.BundleLogger;
+import com.esri.ges.framework.i18n.BundleLoggerFactory;
 
 public class HDFSConnection
 {
-  private String     filenameSuffix;
-  private int        port;
-  private String     filePath;
-  private String     baseFilename;
-  private String     host;
+	private String										filenameSuffix;
+	private int												port;
+	private String										filePath;
+	private String										baseFilename;
+	private String										host;
 
-  Configuration      configuration      = null;
-  Path               path               = null;
-  FileSystem         fileSystem         = null;
-  FSDataOutputStream fsDataOutputStream = null;
-  private long       fileCount          = 0;
-  private long       fileLength;
-  private long       maxEventsPerFile;
-  private URI        uri;
-  private String     user;
+	Configuration											configuration				= null;
+	Path															path								= null;
+	FileSystem												fileSystem					= null;
+	FSDataOutputStream								fsDataOutputStream	= null;
+	private long											fileCount						= 0;
+	private long											fileLength;
+	private long											maxEventsPerFile;
+	private URI												uri;
+	private String										user;
 
-  public HDFSConnection(String host, int port, String filePath, String baseFilename, String filenameSuffix, String user, long maxEventsPerFile) throws IOException
-  {
-    this.host = host;
-    this.port = port;
-    this.filePath = filePath;
-    this.baseFilename = baseFilename;
-    this.filenameSuffix = filenameSuffix;
-    this.maxEventsPerFile = maxEventsPerFile;
-    this.user = user;
+	private static final BundleLogger	LOGGER							= BundleLoggerFactory.getLogger(HDFSConnection.class);
 
-    configuration = new Configuration(false);
-    configuration.setClassLoader(this.getClass().getClassLoader());
-    configuration.set("fs.hdfs.impl", DistributedFileSystem.class.getCanonicalName());
+	public HDFSConnection(String host, int port, String filePath, String baseFilename, String filenameSuffix, String user, long maxEventsPerFile) throws IOException
+	{
+		this.host = host;
+		this.port = port;
+		this.filePath = filePath;
+		this.baseFilename = baseFilename;
+		this.filenameSuffix = filenameSuffix;
+		this.maxEventsPerFile = maxEventsPerFile;
+		this.user = user;
 
-    buildNewPath();
-  }
+		configuration = new Configuration(false);
+		configuration.setClassLoader(this.getClass().getClassLoader());
+		configuration.set("fs.hdfs.impl", DistributedFileSystem.class.getCanonicalName());
 
-  public static void main(final String[] args) throws IOException
-  {
-    String host = "localhost";
-    int port = 8020;
-    if (args.length >= 1)
-      host = args[0];
-    if (args.length >= 2)
-      port = Integer.parseInt(args[1]);
-    System.out.println("Connecting to " + host + ":" + port);
-    HDFSConnection conn = new HDFSConnection(host, port, "/user/cloudera", "gepOutput", "json", "cloudera", 1000000);
-    conn.send("{\"hello\":1234}\n");
-    conn.close();
-  }
+		buildNewPath();
+	}
 
-  public void send(String string) throws IOException
-  {
-    checkFileSystemConnection();
-    fsDataOutputStream.writeBytes(string);
-    fsDataOutputStream.flush();
-    fileLength++;
-  }
+	public static void main(final String[] args) throws IOException
+	{
+		String host = "localhost";
+		int port = 8020;
+		if (args.length >= 1)
+			host = args[0];
+		if (args.length >= 2)
+			port = Integer.parseInt(args[1]);
+		LOGGER.debug("Connecting to " + host + ":" + port);
+		HDFSConnection conn = new HDFSConnection(host, port, "/user/cloudera", "gepOutput", "json", "cloudera", 1000000);
+		conn.send("{\"hello\":1234}\n");
+		conn.close();
+	}
 
-  private void checkFileSystemConnection() throws IOException
-  {
-    if (fsDataOutputStream == null || fileLength >= maxEventsPerFile)
-    {
-      close();
-      fileCount++;
-      fileLength = 0;
-      buildNewPath();
-      while (fileSystem.isFile(path))
-      {
-        fileCount++;
-        buildNewPath();
-      }
-      fsDataOutputStream = fileSystem.create(path);
-    }
-  }
+	public void send(String string) throws IOException
+	{
+		checkFileSystemConnection();
+		fsDataOutputStream.writeBytes(string);
+		fsDataOutputStream.flush();
+		fileLength++;
+	}
 
-  private void buildNewPath() throws IOException
-  {
-    String uriString = "hdfs://" + host + ":" + port + "/" + filePath + "/" + baseFilename + fileCount + "." + filenameSuffix;
-    try
-    {
-      uri = new URI(uriString);
-    }
-    catch (URISyntaxException e)
-    {
-      throw new IOException("could not resolve URI : \"" + uriString + "\"");
-    }
-    path = new Path(uriString);
-    try
-    {
-      if (user != null && user.trim().length() > 0)
-        fileSystem = FileSystem.get(uri, configuration, user);
-      else
-        fileSystem = FileSystem.get(uri, configuration);
-    }
-    catch (InterruptedException e)
-    {
-      throw new IOException("System interrupted while trying to connect to \"" + uriString + "\"");
-    }
-  }
+	private void checkFileSystemConnection() throws IOException
+	{
+		if (fsDataOutputStream == null || fileLength >= maxEventsPerFile)
+		{
+			close();
+			fileCount++;
+			fileLength = 0;
+			buildNewPath();
+			while (fileSystem.isFile(path))
+			{
+				fileCount++;
+				buildNewPath();
+			}
+			fsDataOutputStream = fileSystem.create(path);
+		}
+	}
 
-  public void close()
-  {
-    if (fsDataOutputStream != null)
-      IOUtils.closeStream(fsDataOutputStream);
-    fsDataOutputStream = null;
-  }
+	private void buildNewPath() throws IOException
+	{
+		String uriString = "hdfs://" + host + ":" + port + "/" + filePath + "/" + baseFilename + fileCount + "." + filenameSuffix;
+		try
+		{
+			uri = new URI(uriString);
+		}
+		catch (URISyntaxException e)
+		{
+			throw new IOException(LOGGER.translate("INVALID_URL", uriString));
+		}
+		path = new Path(uriString);
+		try
+		{
+			if (user != null && user.trim().length() > 0)
+				fileSystem = FileSystem.get(uri, configuration, user);
+			else
+				fileSystem = FileSystem.get(uri, configuration);
+		}
+		catch (InterruptedException e)
+		{
+			throw new IOException(LOGGER.translate("CONNECTION_INTERRUPT_ERROR", uriString));
+		}
+	}
+
+	public void close()
+	{
+		if (fsDataOutputStream != null)
+			IOUtils.closeStream(fsDataOutputStream);
+		fsDataOutputStream = null;
+	}
 
 }
--- hdfs-transport/src/main/java/com/esri/geoevent/transport/hdfs/HDFSOutboundTransport.java
+++ hdfs-transport/src/main/java/com/esri/geoevent/transport/hdfs/HDFSOutboundTransport.java
@@ -20,7 +20,7 @@
   Redlands, California, USA 92373
 
   email: contracts@esri.com
-*/
+ */
 
 package com.esri.geoevent.transport.hdfs;
 
@@ -31,190 +31,192 @@ import java.nio.charset.CharacterCodingException;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 import com.esri.ges.core.ConfigurationException;
 import com.esri.ges.core.component.ComponentException;
 import com.esri.ges.core.component.RunningState;
+import com.esri.ges.framework.i18n.BundleLogger;
+import com.esri.ges.framework.i18n.BundleLoggerFactory;
 import com.esri.ges.transport.OutboundTransportBase;
 import com.esri.ges.transport.TransportDefinition;
 
 public class HDFSOutboundTransport extends OutboundTransportBase
 {
-  private static final Log    log                          = LogFactory.getLog(HDFSOutboundTransport.class);
+	private static final BundleLogger	LOGGER												= BundleLoggerFactory.getLogger(HDFSOutboundTransport.class);
 
-  private static final String HOST_NAME_PROPERTY           = "outputHostName";
-  private static final String PORT_PROPERTY                = "outputPort";
-  private static final String FILE_PATH_PROPERTY           = "outputFilePath";
-  private static final String BASE_FILENAME_PROPERTY       = "outputBaseFilename";
-  private static final String FILENAME_SUFFIX_PROPERTY     = "outputFilenameSuffix";
-  private static final String MAX_EVENTS_PER_FILE_PROPERTY = "outputMaxEventsPerFile";
-  private static final String USER_NAME_PROPERTY           = "outputUserName";
+	private static final String				HOST_NAME_PROPERTY						= "outputHostName";
+	private static final String				PORT_PROPERTY									= "outputPort";
+	private static final String				FILE_PATH_PROPERTY						= "outputFilePath";
+	private static final String				BASE_FILENAME_PROPERTY				= "outputBaseFilename";
+	private static final String				FILENAME_SUFFIX_PROPERTY			= "outputFilenameSuffix";
+	private static final String				MAX_EVENTS_PER_FILE_PROPERTY	= "outputMaxEventsPerFile";
+	private static final String				USER_NAME_PROPERTY						= "outputUserName";
 
-  private String              host                         = "localhost";
-  private int                 port                         = 8020;
-  private String              filePath                     = "/user/cloudera";
-  private String              baseFilename                 = "gepOutput";
-  private String              filenameSuffix               = "json";
-  private String              userName                     = null;
-  private int                 maxEventsPerFile             = 10000;
+	private String										host													= "localhost";
+	private int												port													= 8020;
+	private String										filePath											= "/user/cloudera";
+	private String										baseFilename									= "gepOutput";
+	private String										filenameSuffix								= "json";
+	private String										userName											= null;
+	private int												maxEventsPerFile							= 10000;
 
-  private String              errorMessage;
+	private String										errorMessage;
 
-  private HDFSConnection      hdfsClient;
+	private HDFSConnection						hdfsClient;
 
-  private Charset             charset                      = Charset.forName("UTF-8");
-  private CharsetDecoder      decoder;
+	private Charset										charset												= Charset.forName("UTF-8");
+	private CharsetDecoder						decoder;
 
-  public HDFSOutboundTransport(TransportDefinition definition) throws ComponentException
-  {
-    super(definition);
-  }
+	public HDFSOutboundTransport(TransportDefinition definition) throws ComponentException
+	{
+		super(definition);
+	}
 
-  protected void readProperties() throws ConfigurationException
-  {
-    if (hasProperty(HOST_NAME_PROPERTY))
-      host = getProperty(HOST_NAME_PROPERTY).getValueAsString();
-    else
-      host = "localhost";
+	protected void readProperties() throws ConfigurationException
+	{
+		if (hasProperty(HOST_NAME_PROPERTY))
+			host = getProperty(HOST_NAME_PROPERTY).getValueAsString();
+		else
+			host = "localhost";
 
-    if (hasProperty(PORT_PROPERTY))
-      port = ((Integer) getProperty(PORT_PROPERTY).getValue());
-    else
-      port = 27017;
+		if (hasProperty(PORT_PROPERTY))
+			port = ((Integer) getProperty(PORT_PROPERTY).getValue());
+		else
+			port = 27017;
 
-    if (hasProperty(FILE_PATH_PROPERTY))
-    {
-      filePath = getProperty(FILE_PATH_PROPERTY).getValueAsString();
-      while (filePath.length() > 1 && filePath.endsWith("/"))
-        filePath = filePath.substring(0, filePath.length() - 1);
-    }
-    else
-      filePath = "/user/cloudera";
+		if (hasProperty(FILE_PATH_PROPERTY))
+		{
+			filePath = getProperty(FILE_PATH_PROPERTY).getValueAsString();
+			while (filePath.length() > 1 && filePath.endsWith("/"))
+				filePath = filePath.substring(0, filePath.length() - 1);
+		}
+		else
+			filePath = "/user/cloudera";
 
-    if (hasProperty(BASE_FILENAME_PROPERTY))
-      baseFilename = getProperty(BASE_FILENAME_PROPERTY).getValueAsString();
-    else
-      baseFilename = "gepOutput";
+		if (hasProperty(BASE_FILENAME_PROPERTY))
+			baseFilename = getProperty(BASE_FILENAME_PROPERTY).getValueAsString();
+		else
+			baseFilename = "gepOutput";
 
-    if (hasProperty(FILENAME_SUFFIX_PROPERTY))
-      filenameSuffix = getProperty(FILENAME_SUFFIX_PROPERTY).getValueAsString();
-    else
-      filenameSuffix = "json";
+		if (hasProperty(FILENAME_SUFFIX_PROPERTY))
+			filenameSuffix = getProperty(FILENAME_SUFFIX_PROPERTY).getValueAsString();
+		else
+			filenameSuffix = "json";
 
-    if (hasProperty(USER_NAME_PROPERTY))
-      userName = getProperty(USER_NAME_PROPERTY).getValueAsString();
-    else
-      userName = null;
+		if (hasProperty(USER_NAME_PROPERTY))
+			userName = getProperty(USER_NAME_PROPERTY).getValueAsString();
+		else
+			userName = null;
 
-    if (hasProperty(MAX_EVENTS_PER_FILE_PROPERTY))
-      maxEventsPerFile = ((Integer) getProperty(MAX_EVENTS_PER_FILE_PROPERTY).getValue());
-    else
-      maxEventsPerFile = 10000;
+		if (hasProperty(MAX_EVENTS_PER_FILE_PROPERTY))
+			maxEventsPerFile = ((Integer) getProperty(MAX_EVENTS_PER_FILE_PROPERTY).getValue());
+		else
+			maxEventsPerFile = 10000;
 
-  }
+	}
 
-  private void applyProperties() throws IOException
-  {
-    hdfsClient = new HDFSConnection(host, port, filePath, baseFilename, filenameSuffix, userName, maxEventsPerFile);
-  }
+	private void applyProperties() throws IOException
+	{
+		hdfsClient = new HDFSConnection(host, port, filePath, baseFilename, filenameSuffix, userName, maxEventsPerFile);
+	}
 
-  @Override
-  public void afterPropertiesSet()
-  {
-    try
-    {
-      readProperties();
-      if (getRunningState() == RunningState.STARTED)
-      {
-        cleanup();
-        applyProperties();
-      }
-    }
-    catch (Exception ex)
-    {
-      errorMessage = ex.getMessage();
-      log.error(errorMessage);
-      setRunningState(RunningState.ERROR);
-    }
-  }
+	@Override
+	public void afterPropertiesSet()
+	{
+		try
+		{
+			readProperties();
+			if (getRunningState() == RunningState.STARTED)
+			{
+				cleanup();
+				applyProperties();
+			}
+		}
+		catch (Exception error)
+		{
+			errorMessage = error.getMessage();
+			LOGGER.error(errorMessage, error);
+			setRunningState(RunningState.ERROR);
+		}
+	}
 
-  @Override
-  public void receive(ByteBuffer buffer, String channelId)
-  {
-    if (this.getRunningState() == RunningState.STARTED)
-    {
-      try
-      {
-        String json = convertToString(buffer);
-        hdfsClient.send(json);
-      }
-      catch (Exception e)
-      {
-        log.error(e.getMessage(), e);
-      }
-    }
-  }
+	@Override
+	public void receive(ByteBuffer buffer, String channelId)
+	{
+		if (this.getRunningState() == RunningState.STARTED)
+		{
+			try
+			{
+				String json = convertToString(buffer);
+				hdfsClient.send(json);
+			}
+			catch (Exception error)
+			{
+				LOGGER.error("RECEIVE_ERROR", error.getMessage());
+				LOGGER.info(error.getMessage(), error);
+			}
+		}
+	}
 
-  private String convertToString(ByteBuffer buffer)
-  {
-    if (decoder == null)
-      decoder = charset.newDecoder();
-    try
-    {
-      CharBuffer charBuffer = decoder.decode(buffer);
-      String decodedBuffer = charBuffer.toString();
-      return decodedBuffer;
-    }
-    catch (CharacterCodingException e)
-    {
-      log.warn("Could not decode the incoming buffer - " + e);
-      buffer.clear();
-      return null;
-    }
-  }
+	private String convertToString(ByteBuffer buffer)
+	{
+		if (decoder == null)
+			decoder = charset.newDecoder();
+		try
+		{
+			CharBuffer charBuffer = decoder.decode(buffer);
+			String decodedBuffer = charBuffer.toString();
+			return decodedBuffer;
+		}
+		catch (CharacterCodingException error)
+		{
+			LOGGER.error("DECODE_ERROR", error.getMessage());
+			LOGGER.info(error.getMessage(), error);
+			buffer.clear();
+			return null;
+		}
+	}
 
-  private void cleanup()
-  {
-    errorMessage = "";
-    if (hdfsClient != null)
-    {
-      hdfsClient.close();
-      hdfsClient = null;
-    }
-  }
+	private void cleanup()
+	{
+		errorMessage = "";
+		if (hdfsClient != null)
+		{
+			hdfsClient.close();
+			hdfsClient = null;
+		}
+	}
 
-  @Override
-  public synchronized void start()
-  {
-    if (isRunning())
-      return;
-    try
-    {
-      this.setRunningState(RunningState.STARTING);
-      applyProperties();
-      this.setRunningState(RunningState.STARTED);
-    }
-    catch (IOException e)
-    {
-      log.error("Could not start the HDFS output transport : " + e.getMessage());
-      errorMessage = e.getMessage();
-      this.setRunningState(RunningState.ERROR);
-    }
-  }
+	@Override
+	public synchronized void start()
+	{
+		if (isRunning())
+			return;
+		try
+		{
+			this.setRunningState(RunningState.STARTING);
+			applyProperties();
+			this.setRunningState(RunningState.STARTED);
+		}
+		catch (IOException error)
+		{
+			LOGGER.error("START_ERROR", error.getMessage());
+			LOGGER.info(error.getMessage(), error);
+			errorMessage = error.getMessage();
+			this.setRunningState(RunningState.ERROR);
+		}
+	}
 
-  @Override
-  public synchronized void stop()
-  {
-    this.setRunningState(RunningState.STOPPING);
-    cleanup();
-    this.setRunningState(RunningState.STOPPED);
-  }
+	@Override
+	public synchronized void stop()
+	{
+		this.setRunningState(RunningState.STOPPING);
+		cleanup();
+		this.setRunningState(RunningState.STOPPED);
+	}
 
-  @Override
-  public String getStatusDetails()
-  {
-    return errorMessage;
-  }
+	@Override
+	public String getStatusDetails()
+	{
+		return errorMessage;
+	}
 }
